5a1b8c14f6e46ab1bf0bf0bd1042f5ed70ee668b
Mehtab <mehtabatkips@gmail.com>
2025-03-07 05:47:17 +0500

    change bg-white to purple of navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,15 +15,15 @@ const Navbar = () => {
             />
           </div>
 
-          <div className="links w-[50%] bg-white hidden md:flex   justify-between items-center">
-            <a className="text-[2rem]">Home</a>
-            <a className="text-[2rem]">About</a>{" "}
-            <a className="text-[2rem]">Subscription</a>{" "}
-            <a className="text-[2rem]">Contact Us</a>{" "}
+          <div className="links w-[50%]  hidden md:flex text-[2rem]  justify-between items-center">
+            <a className="hover:text-white">Home</a>
+            <a className="hover:text-white">About</a>{" "}
+            <a className="hover:text-white">Subscription</a>{" "}
+            <a className="hover:text-white">Contact Us</a>{" "}
           </div>
         </div>
         {isOpen && (
-          <div className="sidebar w-[50%] h-[80vh]  bg-lightPurple md:hidden flex flex-col justify-between  absolute top-[8rem] left-0 z-50">
+          <div className="sidebar w-[50%] h-[100vh]  bg-lightPurple md:hidden flex flex-col justify-between  absolute top-[8rem] left-0 z-50">
             <div className="grid grid-cols-1 grid-rows-4  bg-lightPurple   h-[40%]">
               <div className="div flex justify-center items-center  border-b-2">
                 <a className="text-[2rem]">Home</a>
